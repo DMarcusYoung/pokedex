e85400168d6bedc6d3f6c74ba6c3052cc5268e03
vkaszuba <codeword.vika@gmail.com>
2020-04-22 19:34:45 -0700

    Page change on yes/no button hit, changes input fields to number only

--- client/src/components/Swipe/Decide/index.js
+++ client/src/components/Swipe/Decide/index.js
@@ -1,16 +1,34 @@
 import React, { Component } from "react";
+import { Link } from 'react-router-dom';
 
 class Decide extends Component {
 
+  state = {
+    restaurant: 0
+  };
+
+  handleIncrement = () => {
+    this.setState({ restaurant: this.state.restaurant + 1 });
+  };
+
+  handleDecrement = () => {
+    this.setState({ restaurant: this.state.restaurant - 1 });
+  };
+
   render() {
     return (
-
       <div>
-        <button className="noBtn">X</button>
-        <button className="yesBtn">O</button>
+
+        <Link to={{pathname: `/swipe/${this.state.restaurant}`}}>
+        <button className="noBtn" onClick={this.handleDecrement}>X</button>
+        </Link>
+
+        <Link to={{pathname: `/swipe/${this.state.restaurant}`}}>
+        <button className="yesBtn" onClick={this.handleIncrement}>O</button>
+        </Link>
       </div>
-    )
+    );
   }
-};
+}
 
 export default Decide;
