d903fb0115e8885474e8a77bfda7e30ba22f3a06
SK <stephanie@sfdigitalstudio.com>
2020-04-23 13:17:39 -0700

    edited models folder

--- models/chickenTinderQueries/chickenTinderQueries.js
+++ models/chickenTinderQueries/chickenTinderQueries.js
@@ -1,9 +1,9 @@
-const createRoom = 'INSERT INTO room (room_number) VALUES (?);';
-const getRoomId = 'SELECT closed FROM room WHERE room_number = ?;';
-const addRestaurant = 'INSERT INTO restaurants (room_number, restaurant_name, restaurant_image_url) VALUES (?);';
-const getRestaurants = 'SELECT restaurant_name, restaurant_image_url FROM restaurants WHERE room_number = ?;';
-const closeRoom = 'UPDATE room SET closed = \'y\' WHERE room_number = ?;';
-const addYes = 'UPDATE restaurant SET num_of_yes = ? WHERE room_number = ? AND restaurant_name = ?;';
+const createRoom = 'INSERT INTO room (room_number) VALUES (?)';
+const getRoomId = 'SELECT closed FROM room WHERE room_number = ?';
+const addRestaurant = 'INSERT INTO restaurants (room_number, restaurant_name, restaurant_image_url) VALUES (?)';
+const getRestaurants = 'SELECT id, restaurant_name, restaurant_image_url, num_of_yes FROM restaurants WHERE room_number = ?';
+const closeRoom = 'UPDATE room SET closed = \'y\' WHERE room_number = ?';
+const addYes = 'UPDATE restaurant SET num_of_yes = ? WHERE id = ?';
 
 
 module.exports = {
