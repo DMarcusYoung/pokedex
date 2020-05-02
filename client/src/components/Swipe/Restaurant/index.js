a7f1210ac596abd4a048e3e34b6305e78b3b6120
vkaszuba <codeword.vika@gmail.com>
2020-05-01 21:41:34 -0700

    Centering and button styling

--- client/src/components/Swipe/Restaurant/index.js
+++ client/src/components/Swipe/Restaurant/index.js
@@ -5,19 +5,20 @@ function Restaurant(props) {
 
   return (
     <div>
-      <div>
+      <div className="titleInfo">
         <h4>{props.rest && props.rest.room_number} - {props.rest && props.rest.city}</h4>
         <h2>{props.rest && props.rest.restaurant_name}</h2>
         <h4 className="stars">{"★".repeat(props.rest && props.rest.rating)}</h4>
       </div>
 
       <div className="choices">
-        <div className="picBox"><img className="picture" src={props.rest && props.rest.restaurant_image_url} alt="" />
+        <div className="picBox">
+          <img className="picture" src={props.rest && props.rest.restaurant_image_url} alt="" />
         </div>
 
 
-        <button className="noBtn" onClick={() => props.handleNo()}>X</button>
-        <button className="yesBtn" onClick={() => props.handleYes(props.rest.id)} >O</button>
+        <button className="noBtn" onClick={() => props.handleNo()}>✘</button>
+        <button className="yesBtn" onClick={() => props.handleYes(props.rest.id)} >✔</button>
       </div>
 
     </div>
