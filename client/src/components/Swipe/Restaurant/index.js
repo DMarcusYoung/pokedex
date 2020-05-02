b375ddb417bd9c721d441b3f69dccf0adb4f019e
vkaszuba <codeword.vika@gmail.com>
2020-05-02 13:25:18 -0700

    Adding alt to yesno buttons

--- client/src/components/Swipe/Restaurant/index.js
+++ client/src/components/Swipe/Restaurant/index.js
@@ -6,25 +6,41 @@ function Restaurant(props) {
   return (
     <div>
       <div className="titleInfo">
-        <h4>{props.rest && props.rest.room_number} - {props.rest && props.rest.city}</h4>
-        <h2>{props.rest && props.rest.restaurant_name}</h2>
-        <h4 className="stars">{"★".repeat(props.rest && props.rest.rating)}</h4>
+        <h4>
+          {props.rest && props.rest.room_number} - {props.rest && props.rest.city}
+        </h4>
+        <h2>
+          {props.rest && props.rest.restaurant_name}
+        </h2>
+        <h4 className="stars">
+          {"★".repeat(props.rest && props.rest.rating)}
+        </h4>
       </div>
 
       <div className="choices">
         <div className="picBorder">
           <div className="picBox">
-            <img className="picture" src={props.rest && props.rest.restaurant_image_url} alt="" />
+            <img className="picture"
+              src={props.rest && props.rest.restaurant_image_url}
+              alt=""
+            />
           </div>
         </div>
 
-        <img src="../../../../images/no.jpg" className="noBtn" onClick={() => props.handleNo()} />
-        <img src="../../../../images/yes.jpg" className="yesBtn" onClick={() => props.handleYes(props.rest.id)} />
-      </div>
+        <img className="noBtn"
+          src="../../../../images/no.jpg"
+          alt="no"
+          onClick={() => props.handleNo()}
+        />
+        <img className="yesBtn"
+          src="../../../../images/yes.jpg"
+          alt="yes"
+          onClick={() => props.handleYes(props.rest.id)}
+        />
 
+      </div>
     </div>
   )
-
 }
 
 export default Restaurant;
