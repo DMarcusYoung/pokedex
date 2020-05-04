7a813503b9b3b97d880ecea070fa562248c74040
vkaszuba <codeword.vika@gmail.com>
2020-05-03 18:44:00 -0700

    Match page style fixed, button added to Yelp page

--- client/src/components/Match/index.js
+++ client/src/components/Match/index.js
@@ -10,7 +10,7 @@ class Match extends Component {
   }
 
   async componentDidMount() {
-    try { 
+    try {
       const { data: restaurant } = await axios.get(`/api/restaurant/1/${this.props.match.params.restId}`);
       this.setState({ restaurant });
     } catch (e) {
@@ -18,33 +18,45 @@ class Match extends Component {
     }
   }
 
+
   render() {
     console.log(this.state);
     return (
 
       <div className="card">
         <Link to="/lobby">
-  
-          <h1 className="animated heartBeat slow">you have a match!</h1>
-  
+
+
+          <h3 className="matchTitle animated heartBeat slow">you have a match!</h3>
+
+          <div className="matchChoices">
           <h2 className="animated fadeIn delay-1s slower">
             {this.state.restaurant.restaurant_name}
           </h2>
-  
-          <h3 className="animated fadeIn delay-1s slower">
-            {this.state.restaurant.rating}
-          </h3>
-  
-          <div className="choices animated fadeIn delay-1s slower">
+
+          <h4 className="stars animated fadeIn delay-1s slower">
+            {"★".repeat(this.state.restaurant.rating)}
+          </h4>
+
+
+          <div className="animated fadeIn delay-1s slower">
             <div className="picBorder">
               <div className="picBox">
-                <img className="picture" src={this.state.restaurant.restaurant_image_url} alt="" />
+                <img className="picture"
+                src={this.state.restaurant.restaurant_image_url}
+                alt="" />
               </div>
             </div>
           </div>
-  
-        </Link>
-  
+          </div>
+          </Link>
+
+          <a className="backBtn"
+          href={this.state.restaurant.rest_url}
+          target="_blank"
+          >details</a>
+
+
       </div>
     )
   }
